5c4e89466ab71138d387a7016d5c568bd54ffdd6
vikas kumawat <42940952+Vikaskumawat2018@users.noreply.github.com>
2024-10-10 19:58:46 +0545

    ex:-pass exposed

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ dotenv.config();
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: "test",
     database: 'vuln_app'
 });
 
